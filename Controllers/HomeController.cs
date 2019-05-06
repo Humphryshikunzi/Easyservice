b7533aca681180a5d5b5117954660560d7bcbb40
Shiks <humphryshikunzi9@gmail.com>
2019-05-06 14:32:50 +0300

    Files for Easyservice succesfully added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Easyservice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+     
+        public ActionResult Signinpage()
+        {
+            return View();
+        }
+
+        public ActionResult Signuppage()
+        {
+            return View();
+        }
+
+        public ActionResult Complaints()
+        {
+            return View();
+        }
+        public ActionResult Support()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int? NationalIdentification { get; set; }
+        public double? Remainingfeebalance { get; set; }
+
+
+    }
+}
