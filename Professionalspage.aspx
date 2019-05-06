42ea3b62781314927c76c3b501c6b01fdb8b3e02
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2019-05-06 14:44:02 +0300

    Add files via upload
    The three pages are for professionals page which am yet to add to the project

---
+++ Professionalspage.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 150px;
+        }
+    </style>
+</head>
+<body>
+<form id="form1" runat="server">
+    <!-- <div>
+        <h4>Shiks Students Data</h4>
+        <asp:ListBox runat="server" ID="NamesListBox1" Height="800" DataTextField="Name" DataValueField="Name"/>
+    </div>-->
+   
+    <table>
+        <tr>
+            <td class="auto-style1">Names</td>
+            <td>Registration Numbers</td>
+            <td>National Ids</td>
+            <td>Remaining Fee Balances</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">
+                <asp:ListBox runat="server" ID="NamesListBox" Height="800" DataTextField="Name" DataValueField="Name"/>
+            </td>
+            <td class="auto-style1">
+                <asp:ListBox runat="server" ID="RegNumbersListBox" Height="800" DataTextField="RegistrationNumber" DataValueField="RegistrationNumber"/>
+            </td>
+            <td class="auto-style1">
+                <asp:ListBox runat="server" ID="NationalIdListBox" Height="800" DataTextField="NationalIdentification" DataValueField="NationalIdentification"/>
+            </td>
+            <td class="auto-style1">
+                <asp:ListBox runat="server" ID="RemainFeeBalListBox" Height="800" DataTextField="Remainingfeebalance" DataValueField="Remainingfeebalance"/>
+            </td>
+        </tr>
+     
+    </table>
+
+</form>
+</body>
+</html>
---
+++ Professionalspage.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+        
+        HttpClient httpClient = new HttpClient();
+            var jsondata = await httpClient.GetStringAsync("https://shiksstudentsapi.azurewebsites.net/api/Students");
+            var listdata = JsonConvert.DeserializeObject<List<Students>>(jsondata);
+
+            Dictionary<Students, int> student = new Dictionary<Students, int>();
+            for (int i = 0; i < listdata.Count; i++)
+            {
+                if ((listdata[i].Name != null) && (listdata[i].NationalIdentification != 0)
+                                               && (listdata[i].RegistrationNumber != null) &&
+                                               (listdata[i].Remainingfeebalance != null))
+                {
+                    student.Add(new Students()
+                    {
+                        Name = listdata[i].Name,
+                        RegistrationNumber = listdata[i].RegistrationNumber,
+                        Remainingfeebalance = listdata[i].Remainingfeebalance,
+                        NationalIdentification = listdata[i].NationalIdentification
+                    }, listdata[i].Id);
+                }
+            }
+
+            List<Students> names = new List<Students>();
+            List<Students> nationalids = new List<Students>();
+            List<Students> regnums = new List<Students>();
+            List<Students> remfeebals = new List<Students>();
+            foreach (KeyValuePair<Students, int> std in student.OrderBy(i => i.Key.Name))
+            {
+                names.Add(new Students() { Name = std.Key.Name });
+                names.Add(new Students() { Name = null });
+
+            }
+            foreach (KeyValuePair<Students, int> std in student.OrderBy(i => i.Key.Name))
+            {
+                nationalids.Add(new Students() { NationalIdentification = std.Key.NationalIdentification });
+                nationalids.Add(new Students() { NationalIdentification = null });
+
+            }
+            foreach (KeyValuePair<Students, int> std in student.OrderBy(i => i.Key.Name))
+            {
+                remfeebals.Add(new Students() { Remainingfeebalance = std.Key.Remainingfeebalance });
+                remfeebals.Add(new Students() { Remainingfeebalance = null });
+
+            }
+            foreach (KeyValuePair<Students, int> std in student.OrderBy(i => i.Key.Name))
+            {
+                regnums.Add(new Students() { RegistrationNumber = std.Key.RegistrationNumber });
+                regnums.Add(new Students() { RegistrationNumber = null });
+
+            }
+
+            NamesListBox.DataSource = names;
+            NamesListBox.DataBind();
+            RegNumbersListBox.DataSource = regnums;
+            RegNumbersListBox.DataBind();
+            RemainFeeBalListBox.DataSource = remfeebals;
+            RemainFeeBalListBox.DataBind();
+            NationalIdListBox.DataSource = nationalids;
+            NationalIdListBox.DataBind();
+        }
+
+
+    }
+}
